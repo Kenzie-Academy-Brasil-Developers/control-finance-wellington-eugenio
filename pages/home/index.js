f990e6000b564fd185a258b0b71b228485093431
wellington-eugenio <wellington.eliteinfo@gmail.com>
2022-10-02 21:27:59 -0300

    inicialize modal

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,1 +1,63 @@
 /* Desenvolva sua lógica aqui */
+/*<li class="card flex space center">
+                <p>R$ 00,00</p>
+                <div class="id-remove flex space center">
+                    <p class="tipo">Entrada</p>
+                    <button id="remove"><i class="fa-solid fa-trash-can"></i></button>                
+                </div>
+</li>
+*/
+let formatoMoeda = { minimumFractionDigits: 2 , style: 'currency', currency: 'BRL' }
+
+let countId = 0
+let some = 0
+
+function createCards(objeto){
+    const valorDocard = parseInt(objeto.value);
+    const tipo  = objeto.categoryID;
+    const idObjeto = objeto.id
+    
+    const cardBody       = document.createElement('li');
+    cardBody.classList   = 'card flex space center';
+
+    const valueCArd      = document.createElement('p');
+    valueCArd.innerText  = valorDocard.toLocaleString('pt-BR', formatoMoeda)
+    
+    const cardcontai     = document.createElement('div');
+    cardcontai.classList = 'id-remove flex space center';
+
+    const cardType       = document.createElement('p');
+    cardType.classList   = 'tipo';
+    cardType.innerText   = tipo;
+
+    const cardbutton     = document.createElement('button');
+    cardbutton.classList = "remove"
+    cardbutton.id        = `re-${idObjeto}`;
+    cardbutton.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+    
+    cardbutton.addEventListener('click',(event)=>{
+        let li = event.path[3]
+        li.remove()
+        some -= valorDocard
+        document.querySelector('#total').innerHTML = some.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+        insertedValues.splice(idObjeto, 1)
+    })
+    
+    cardcontai.append(cardType, cardbutton)
+
+    cardBody.append(valueCArd, cardcontai)
+
+    return cardBody;
+}
+
+const cardMain = document.querySelector('.list-entries')
+
+function listCards(){  
+    cardMain.innerHTML = ""
+    insertedValues.forEach((obj)=>{
+        let objAtual = createCards(obj)
+        cardMain.appendChild(objAtual)
+    })
+
+}
+
--- scripts/modal.js
+++ scripts/modal.js
@@ -1,1 +1,184 @@
 /* Desenvolva sua lógica aqui */
+function createModal(){
+    const modalCorp       = document.createElement('div');
+    modalCorp.classList   = 'modal';
+    modalCorp.id          = 'md-cp'
+    
+    const modalBody       = document.createElement('section');
+    modalBody.classList   = 'modal-container flex column space';
+
+    const modalReader     = document.createElement('div');
+    modalReader.classList = 'modal-reader flex space center';
+
+    const modalTitle      = document.createElement('h3');
+    modalTitle.classList  = 'modal-title';
+    modalTitle.innerText  = 'Registro de valor';
+
+    const closeModal      = document.createElement('button');
+    closeModal.classList  = 'exit';
+    closeModal.id         = 'ex';
+    closeModal.innerText  = 'X';
+
+    const modalText       = document.createElement('p');
+    modalText.innerText   = 'Digite o valor e em seguida aperte no botão referente ao tipo do valor';
+
+    const modalForm       = document.createElement('form');
+    modalForm.classList   = 'modal-form flex column space';
+
+    const modalFormtitle  = document.createElement('label');
+    modalFormtitle.classList = 'label-enti';
+    modalFormtitle.innerText = 'Valor';
+
+    const modalInput      = document.createElement('div');
+    modalInput.classList  = 'value-in flex center';
+    
+    const inputText       = document.createElement('p');
+    inputText.innerText   = 'R$';
+
+    const inputForm       = document.createElement('input');
+    inputForm.type        = 'text';
+    inputForm.name        = 'value';
+    inputForm.classList   = 'input-valor';
+    inputForm.required    = true
+    inputForm.placeholder = '00,00';
+
+    const valorTipo       = document.createElement('div');
+    valorTipo.classList   = 'modal-tipo flex center';
+
+    const tipoText        = document.createElement('p');
+    tipoText.innerText    = 'Tipo de valor';
+
+    const radio1          = document.createElement('input');
+    radio1.type           = 'radio';
+    radio1.classList      = 'input-radio';
+    radio1.name           = 'categoryID';
+    radio1.required       = true;
+    radio1.id             = 'entrada';
+    radio1.value          = 'Entrada';
+    
+    const radio1label     = document.createElement('label');
+    radio1label.htmlFor   = 'entrada'
+    radio1label.classList = 'input-radio-name';
+    radio1label.innerText = 'Entrada';
+
+    const radio2          = document.createElement('input');
+    radio2.type           = 'radio';
+    radio2.classList      = 'input-radio';
+    radio2.name           = 'categoryID';
+    radio2.id             = 'saida';
+    radio2.value          = 'Saida';
+    
+    const radio2label     = document.createElement('label');
+    radio2label.htmlFor   = 'saida';
+    radio2label.classList = 'input-radio-name';
+    radio2label.innerText = 'Saída';
+
+    const modalActions     = document.createElement('div');
+    modalActions.classList = 'modal-actions flex center';
+
+    const modalCancel      = document.createElement('button');
+    modalCancel.classList  = 'button-cancel';
+    modalCancel.id         = 'ex';
+    modalCancel.innerText  = 'Cancelar';
+
+    const modalInsert      = document.createElement('button');
+    modalInsert.classList  = 'button-insert';
+    modalInsert.id         = 'insert';
+    modalInsert.type       = 'submit';
+    modalInsert.innerText  = 'Inserir Valor';
+
+    modalForm.addEventListener('submit', (event)=>{
+        event.preventDefault();
+        countId++
+        console.log(countId)
+        let newEntry = {}
+        newEntry.id = countId
+        const forSubmit = [...event.target]
+
+        forSubmit.forEach((entry)=>{
+            if(entry.value && entry.type !== 'radio'){
+                newEntry[entry.name] = entry.value
+            }else if(entry.checked){
+                newEntry[entry.name] = entry.value
+            }
+        })
+        insertedValues.push(newEntry)
+        listCards()
+        some += parseInt(newEntry.value)
+        document.querySelector('#total').innerHTML = some.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+        modalCorp.remove()
+        
+    })
+
+    modalReader.append(modalTitle, closeModal);
+
+    modalInput.append(inputText, inputForm);
+
+    valorTipo.append(tipoText, radio1, radio1label, radio2, radio2label);
+
+    modalActions.append(modalCancel, modalInsert);
+
+    modalForm.append(modalFormtitle, modalInput, valorTipo, modalActions);
+
+    modalBody.append(modalReader, modalText, modalForm);
+
+    modalCorp.appendChild(modalBody);
+
+    return modalCorp;
+}
+
+function showModal(){
+    const buttonShow = document.getElementById('new');
+    const bodyLocal  = document.getElementById('body');
+
+    buttonShow.addEventListener('click',()=>{
+        const modal = createModal();
+        
+        bodyLocal.appendChild(modal);
+        exitModal()
+
+    })
+}
+
+function exitModal(){
+    const buttonClose = document.getElementById('ex');
+    const modalCorpo  = document.getElementById('md-cp');
+
+    buttonClose.addEventListener('click',()=>{
+        modalCorpo.remove()
+    })
+}
+
+showModal()
+
+/*<div class="modal">
+<section class="modal-container flex column space">
+<div class="modal-reader flex space center">
+    <h3 class="modal-title">Registro de valor</h3>
+    <button class="exit" id="ex">X</button>
+</div>
+<p>Digite o valor e em 
+    seguida aperte no botão referente
+    ao tipo do valor
+</p>
+<form class="modal-form flex column space" action="">
+    <label  class="label-enti" for="">Valor</label>
+    <div class="value-in flex center">
+        <p>R$</p>
+        <input class="input-valor" type="text" placeholder="00,00">
+    </div>
+    <div class="modal-tipo flex center">
+        <p>Tipo de valor</p>
+        <input class="input-radio" type="radio" name="option" id="entrada" value="1">
+        <label class="input-radio-name" for="entrada">Entrada</label>
+        <input class="input-radio" type="radio" name="option" id="saida" value="2">
+        <label class="input-radio-name" for="saida">Saída</label>               
+    </div>
+    <div class="modal-actions flex center">
+        <button class="button-cancel" id="ex" >Cancelar</button>
+        <button class="button-insert" type="submit" id="insert">Inserir valor</button>
+    </div>    
+</form>     
+</section>
+</div>*/
+
